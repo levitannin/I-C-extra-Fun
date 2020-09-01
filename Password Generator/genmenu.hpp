96ab8c538ce9359102d56275f3f698f9961bae61
Levi <27644153+levitannin@users.noreply.github.com>
2020-09-01 07:23:36 -0400

    Create genmenu.hpp

---
+++ Password Generator/genmenu.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+//Header file
+#include "tools.hpp"
+#include "platform_pass.hpp"
+#include "serverpass.hpp"
+#include "controllerpass.hpp"
+
+//Prototypes
+void downloads();
+void menu1();
+void servmenu();
+void controlmenu();
